1c9915c0f136659e96105eaa13b7d56f1aaed86d
Usmon <usmonpahlavonov@gmail.com>
2025-05-26 15:19:33 +0900

    init

---
+++ src/main/java/com/up/balance_acc/BalanceAccApplication.java
@@ -0,0 +1,13 @@
+package com.up.balance_acc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceAccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceAccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/up/balance_acc/BalanceAccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.up.balance_acc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceAccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
